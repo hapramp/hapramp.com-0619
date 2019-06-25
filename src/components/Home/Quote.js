aa937f90b7d4ddb626bfae3fb57d90d9f7ce8da4
ansarimofid <ansarimofid@gmail.com>
2019-06-25 15:42:32 +0530

    Adds quote section

---
+++ src/components/Home/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Quote = () => {
+  return (
+    <section className="quote my-32">
+      <div className="max-w-4xl mx-auto font-light text-4xl">
+      “Your data is more worth than you might expect, in the future, it might become your lifeline.”<span className="font-display font-bold"> — Jaron Lanier</span>
+      </div>
+    </section>
+  );
+};
+
+export default Quote;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Hero';
 import Hero from './Hero';
 import Feature from './Feature'
+import Quote from './Quote'
 
 const index = () => {
   return (
    <main>
      <Hero />
      <Feature />
+     <Quote />
    </main>
   );
 };
