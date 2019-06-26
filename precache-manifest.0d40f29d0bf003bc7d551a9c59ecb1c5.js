b68ab5b005b1ba685193466183ed043964c7ade6
ansarimofid <ansarimofid@gmail.com>
2019-06-26 15:09:22 +0530

    Updates

---
+++ precache-manifest.0d40f29d0bf003bc7d551a9c59ecb1c5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8947368db3c40f18fbac55e8ded0609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7603715e3f12fa74af8",
+    "url": "/static/css/main.0293c1c3.chunk.css"
+  },
+  {
+    "revision": "702f4c18ed292b3bec63",
+    "url": "/static/js/2.22c2fbdf.chunk.js"
+  },
+  {
+    "revision": "e7603715e3f12fa74af8",
+    "url": "/static/js/main.6d9cc1bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "958a01f704004193f4e7f6387c195b35",
+    "url": "/static/media/ankit_hapramp.958a01f7.jpg"
+  },
+  {
+    "revision": "5b4b33fa68c6df2bda4f13600e88e9d8",
+    "url": "/static/media/bg-app.5b4b33fa.png"
+  },
+  {
+    "revision": "d4f663508c83b2a2dda74e129e4ebbe3",
+    "url": "/static/media/hiring.d4f66350.svg"
+  },
+  {
+    "revision": "2d1404abd2076c62d252b5f8a98190a9",
+    "url": "/static/media/iiitv.2d1404ab.png"
+  },
+  {
+    "revision": "eed91d14dcfbaa28ed0907b3d234095a",
+    "url": "/static/media/jaspreet.eed91d14.jpg"
+  },
+  {
+    "revision": "271d6b7fd7885d7e34ccd5afefd90520",
+    "url": "/static/media/linkedin.271d6b7f.svg"
+  },
+  {
+    "revision": "ed268187a3f1fa3f9e5d695e83023004",
+    "url": "/static/media/mofid_hapramp.ed268187.jpg"
+  },
+  {
+    "revision": "856ad858e821495cd0de408385999a1b",
+    "url": "/static/media/nascomm.856ad858.png"
+  },
+  {
+    "revision": "3c9b841295af3e95203395d032b37cde",
+    "url": "/static/media/oneRamp.3c9b8412.png"
+  },
+  {
+    "revision": "5169dcad50bbf37ca7f2f1ca8e5b0157",
+    "url": "/static/media/pratyush_hapramp.5169dcad.jpg"
+  },
+  {
+    "revision": "b96f5b57a10498d263727292bbc6f59d",
+    "url": "/static/media/rajat_hapramp.b96f5b57.jpg"
+  },
+  {
+    "revision": "ef0820dc15f2b4a561ba9b21aa6af70e",
+    "url": "/static/media/twitter.ef0820dc.svg"
+  },
+  {
+    "revision": "752dbd7c91047565d207a169697f1835",
+    "url": "/static/media/vikram_hapramp.752dbd7c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hapramp-0619/precache-manifest.61a61215c10834ec36dfc5bfaa107f5c.js"
+  "/precache-manifest.0d40f29d0bf003bc7d551a9c59ecb1c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hapramp-0619/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
