3c240adec5885d5927d1164a0b5b8969d2588ea0
ansarimofid <ansarimofid@gmail.com>
2019-09-26 18:05:44 +0530

    Adds 404.html, scrollToTop and meta data

--- src/App.js
+++ src/App.js
@@ -10,17 +10,22 @@ import Home from './components/Home';
 import About from './components/About';
 import Work from './components/Work';
 
+import ScrollToTop from './ScrollToTop';
+
 function App() {
   return (
     <div className="font-body antialiased">
       <BrowserRouter >
         <Header />
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/asteria" component={Asteria}/>
-          <Route path="/about" component={About} />
-          <Route path="/work" component={Work} />
-        </Switch>
+        <ScrollToTop>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/asteria" component={Asteria}/>
+            <Route path="/about" component={About} />
+            <Route path="/work" component={Work} />
+          </Switch>
+        </ScrollToTop>
+        
         <Footer />
       </BrowserRouter>
      
