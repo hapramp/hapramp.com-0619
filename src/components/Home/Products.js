e8cf4be502089181253341b9df096f10e7861c55
ansarimofid <ansarimofid@gmail.com>
2020-06-19 13:28:42 +0530

    Updates text

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -14,7 +14,7 @@ const Products = () => {
         <div className="md:flex mt-10 sm:mt-12 md:mt-16 border-b pb-10">
           <img src={gosocial} alt="Go Social App Logo"/>
           <div className="md:ml-12">
-            <h3 className="text-4xl font-display mt-4 md:mt-0">Go.Social</h3>
+            <h3 className="text-4xl font-display mt-4 md:mt-0">GoSocial</h3>
             <p className="mt-4 text-lg">Inspiring humans to take on a digital journey of their lifetime. A platform where you learn, explore, create, and grow as a creative individual.</p>
             <a className="mt-8 inline-block underline font-bold" href="https://gosocialapp.co" target="_blank" rel="noopener noreferrer">LET’S GO</a>
           </div>
